0b910d4c537f6a0f5816e4a7238432daae342b85
NTN <ntnguyenit@live.com>
2016-08-15 12:04:04 +0700

    add plugin config

--- models/Configuration.php
+++ models/Configuration.php
@@ -1,6 +1,7 @@
 <?php namespace Ntn\Configuration\Models;
 
 use October\Rain\Database\Model;
+use Illuminate\Support\Facades\Cache;
 
 class Configuration extends Model
 {
@@ -21,5 +22,31 @@ class Configuration extends Model
         'value' => 'required',
         'description' => 'required',
     ];
+
+    /**
+     * Get value of configuration by key
+     * @param $key
+     * @return string
+     */
+    public static function getValueByKey($key, $langCode = null)
+    {
+        $cacheKey = __FUNCTION__ . ':' . $key . '.' . $langCode;
+//        $result = Cache::tags('configuration')->get($cacheKey);
+        $result = Cache::get($cacheKey);
+
+        if ($result == null) {
+            if ($langCode) {
+                $key .= '_' . $langCode;
+            }
+            $config = self::where('key', strtoupper($key))->first();
+            if ($config) {
+
+                $result = $config->value;
+//                Cache::tags('configuration')->forever($cacheKey, $result);
+                Cache::forever($cacheKey, $result);
+            }
+        }
+        return $result;
+    }
 
 }
